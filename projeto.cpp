8169a7eb1eb26681a5c2f20893564d9d7cb7ce71
HAHAsávioviado <savioviado@gmail.com>
2019-11-01 18:28:01 -0200

    haha

--- projeto.cpp
+++ projeto.cpp
@@ -1,81 +1,35 @@
-<<<<<<< HEAD
-<<<<< HEAD
-=======
-<<<<<<< HEAD
-#include <iostream>
-<<<<<<< HEAD
-<<<<<<< HEAD
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
+PAREM COM ISSO MANO 
 
-using namespace std;
-
-void naosei(){
-cout<<"Ai,meu Deus";
-}
-=======
-usin namespace std;
-
-void funcao_Mylena(){
-    cout<<"Meu nome eh Mylena";
->>>>>>> 5ffe7a1703b6dd21684511b8e5050d30da49d06e
-=======
-using namespace std;
-<<<<<<< HEAD
-int ian()
-{
-	int i;
-	i=i+1;
-}
-=======
-
-void funcao_caua (){
-	cout<<"me chamo caua"
-<<<<<<< HEAD
->>>>>>> ebc226f5448582e7e27f25a0a018873092270163
-=======
->>>>>>> 94edf10a18957db0b7f911b8753117be9be1c077
->>>>>>> 5ffe7a1703b6dd21684511b8e5050d30da49d06e
-}
-
-int main(){
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-naosei();
-  cout << "OlÃ¡ mundo!";
-=======
-  cout << "OlÃ¡ mundo!";
-  funcao_Mylena()
->>>>>>> 5ffe7a1703b6dd21684511b8e5050d30da49d06e
-
-=======
-	cout << "OlÃ¡ mundo!";
-	funcao_caua();
->>>>>>> ebc226f5448582e7e27f25a0a018873092270163
-<<<<<<< HEAD
-=======
->>>>>>> 94edf10a18957db0b7f911b8753117be9be1c077
->>>>>>> 5ffe7a1703b6dd21684511b8e5050d30da49d06e
-=======
->>>>>>> ebc226f5448582e7e27f25a0a018873092270163
-#include<iostream>
-#include<string>
-using namespace std;
->>>>>>> 7a846070e9dcca535b340e9f378427aba3e2c23e
-int main(){
-	int cod;
-	string nome;
-	cout<<"Digite código do corno:";
-	cin>>cod;
-	while(cod!=0)
-	{
-		cout<<"Digite quantas vezes foi corno e sabe:";
-		cin>>vezes;
-		if(vezes>=2)
-			cout<<"Tu tá fudido parceiro, sua mulher/homem tá com AIDs"
-	}
-	cout << "Olá¡ mundo!";
-	return 0;
->>>>>>> 7a846070e9dcca535b340e9f378427aba3e2c23e
-}
-=====
-#include<>
